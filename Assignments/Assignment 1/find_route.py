91424ba79d6650d990bfe5e6bbf589dae12a3332
Mahesh Koppala <55094217+mahesh-koppala@users.noreply.github.com>
2020-02-17 00:10:14 -0600

    Add files via upload

--- Assignments/Assignment 1/find_route.py
+++ Assignments/Assignment 1/find_route.py
@@ -1,200 +1,180 @@
-import sys
-from collections import deque
+#This is sample code for Assignment 1, Task 1
+#(c) Vamsikrishna Gopikrishna, PhD.
+#Dept. Of Computer Science and Engineering
+#Univerity of Texas at Arlington
+import sys #To extract cmd line arguments and for exit func
 
-class Node:
-    def __init__(self, name, cumulative, d, f):
-        self.name = name
-        self.parents = None
-        self.cumulative = cumulative
-        self.d = d
-        self.f = f
-        
-        
-class informed_search:
-    
-    
-    def find_route(self, start, target):
-        
-        
-           z=0
-           input_file = sys.argv[1]
-           Heuristic_file = sys.argv[4]
-           f = open(input_file, "r")
-           h = open(Heuristic_file, "r")
-           PATH = []
-           Heuristic = []
-           V = []
-           for line in h:
-               if "END OF INPUT" in line:
-                   break
-               else:
-                   Hu = line.split()
-                   heuristic_city = Hu[0]
-                   heuristic_num = Hu[1]
-                   Heuristic.append([heuristic_city, heuristic_num])
-           for line in f:
-               if "END OF INPUT" in line:
-                   break
-               else:
-                   City = line.split()
-                   Src = City[0]
-                   Destination = City[1]
-                   distance = City[2]
-                   path1 = [Src, Destination, distance]
-                   path2 = [Destination, Src, distance]
-                   PATH.append(path1)
-                   PATH.append(path2)
-           Queue = deque()
-           Queue.append(Node(start, 0, 0, 0))
-           nodes_generated=0
-           no_target = False
-           while True:
-               if len(Queue) != 0:
-                   global w
-                   if(len(Queue)>w):
-                       w = len(Queue)
-                   nExpected = Queue.popleft()
-                   z=z+1
-                   nodes_generated += 1
-                   if nExpected.name == target:
-                       break
-                   else:
-                       if nExpected.name not in V:
-                           V.append(nExpected.name)
-                           for num in PATH:
-                               if num[0] == nExpected.name:
-                                   for hu in Heuristic:
-                                       if hu[0] == num[1]:
-                                           Hunum=hu[1]
- 
-                                   n = Node(num[1], nExpected.cumulative+int(num[2]), nExpected.d+1, nExpected.cumulative+int(num[2])+int(Hunum))
-                                   n.parents= nExpected
-                                   Queue.append(n)
-                                   s=sorted(Queue, key=lambda node:node.f)
-                                   Queue=deque(s)
-               else:
-                   no_target = True
-                   break
-           z=z+len(Queue)
-           if(no_target == True):
-               print ("Nodes Expanded: "+str(nodes_generated))
-               print("Nodes Generated:" +str(z-1))
-               print("Max Nodes In Memory:" +str(w))
-               print ("Distance: Infinity")
-               print ("Route: None")
-           else:
-               print ("Nodes Expanded: "+str(nodes_generated))
-               print("Nodes Generated:" +str(z-1))
-               print("Max Nodes In Memory:" +str(w))
-               print ("Distance: " + str(nExpected.cumulative))
-               print ("Route: ")
-               path = []
-               path.append(target)
-               while nExpected.parents != None:
-                   path.append(nExpected.parents.name)
-                   nExpected = nExpected.parents
-               path.reverse()
-               i=0
-               while i < len(path)-1:
-                   for v in PATH:
-                       if path[i]==v[0] and path[i+1] == v[1]:
-                           print (v[0] +" to "+ v[1]+ " - "+v[2])
-                   i += 1
+class node:
+	def __init__(self, parent, state, g, d, f, uSearch):
+		self.parent = parent
+		self.state = state
+		self.g = g
+		self.d = d
+		self.f = f
+		self.uSearch = uSearch #this is set to true for Uininformed search and false for not
+	
+	def __str__(self): #this is used to convert a node to string format, if necessary (for dumping the fringe)
+		if self.uSearch:
+			return '{ '+self.state+': g(n)='+ str(self.g)+', d='+str(self.d)+', Parent--> <'+str(self.parent)+'> }' 
+		else:
+			return '{ '+self.state+': g(n)='+ str(self.g)+', d='+str(self.d)+', f(n)='+str(self.f)+', Parent--> <'+str(self.parent)+'> }'
+	
+	def __cmp__(self,other): #this is used to make the fringe sortable for UCS or A* (for uninf uses g and for inf uses f)
+		if self.uSearch:
+			return cmp(self.g,other.g)
+		else:
+			return cmp(self.f,other.f)
 
+def dump(fringe, closed): #dumps both fringe and closed to std output
+	print 'Fringe: '
+	for n in fringe:
+		print '\t'+str(n)
+	print 'Closed: \n\t'+str(closed)+'\n'
+	
+def expand(n,m,h): #uses map and heuristic (if needed) to generate list of succesor nodes for a given node
+	succ = []
+	acts = m[n.state]
+	for a in acts:
+		cCost = n.g + a[1]
+		if n.uSearch:
+			succ.append(node(n, a[0], cCost, n.d + 1, 0,n.uSearch)) #uninformed search does not use it so set f to 0
+		else:
+			succ.append(node(n, a[0], cCost, n.d + 1, cCost + h[a[0]],n.uSearch))
+	return succ
 
+def readInput(fName): #Reads the input file and creates a dictionary. Stops on reaching line END OF INPUT
+	f = open(fName,'r')
+	m = {}
+	for l in f:
+		l = l.rstrip('\n')
+		l = l.rstrip('\r')
+		if l == 'END OF INPUT':
+			f.close()
+			return m
+		else:
+			v = l.split(' ')
+			if v[0] in m: #handle city 1 --> city 2
+				m[v[0]].append([v[1],float(v[2])])
+			else:
+				m[v[0]] = [[v[1],float(v[2])]]
+			if v[1] in m: #handle city 2 --> city 1
+				m[v[1]].append([v[0],float(v[2])])
+			else:
+				m[v[1]] = [[v[0],float(v[2])]]
+	print 'This part is unreachable. If you are seeing this check Input File Formatting'
 
+def readHeuristic(fName): #Reads the heuristic file and creates a dictionary. Stops on reaching line END OF INPUT
+	f = open(fName,'r')
+	h = {}
+	for l in f:
+		l = l.rstrip('\n')
+		l = l.rstrip('\r')
+		if l == 'END OF INPUT':
+			f.close()
+			return h
+		else:
+			v = l.split(' ')
+			h[v[0]] = float(v[1])
+	print 'This part is unreachable. If you are seeing this check Heuristic File Formatting'
 
+def genRoute(n,m): #reconstruct the path from node
+	dist = n.g
+	steps = []
+	while n is not None:
+		p = n.parent
+		if p is not None:
+			act = (a for a in m[p.state] if a[0] == n.state) #of all the actions of parent find the one that will obtain current state
+			a = act.next()
+			steps.append(p.state+' to '+n.state+', '+str(a[1])+' km')
+		n = p
+	steps.reverse()#since links go from goal to start, reverse the list
+	print '\ndistance: '+str(dist)+' km'
+	for s in steps:
+		print s
 
-
-
-
-class uninformed_search:
-    
-    
-       def find_route(self, start, target):
-          
-          
-           z=0
-           input_file = sys.argv[1] 
-           f = open(input_file, "r")
-           PATH = []
-           V = []
-           for line in f:
-               if "END OF INPUT" in line:
-                   break
-               else:
-                   City = line.split()
-                   Src = City[0]
-                   Destination = City[1]
-                   distance = City[2]
-                   path1 = [Src, Destination, distance]
-                   path2 = [Destination, Src, distance]
-                   PATH.append(path1)
-                   PATH.append(path2)
-           Queue = deque()
-           Queue.append(Node(start, 0, 0, None)) 
-           nodes_generated = 0
-           no_target = False
-           while True:
-               if len(Queue) != 0:
-                   global w
-                   if(len(Queue)>w):
-                       w = len(Queue)
-                   nExpected = Queue.popleft() 
-                   z=z+1
-                   nodes_generated += 1
-                   if nExpected.name == target: 
-                       break
-                   else:
-                       if nExpected.name not in V: 
-                           V.append(nExpected.name) 
-                           for num in PATH:
-                               if num[0] == nExpected.name:
-                                   n = Node(num[1], nExpected.cumulative+int(num[2]), nExpected.d+1, None)
-                                   n.parents= nExpected
-                                   Queue.append(n)
-                                   s=sorted(Queue, key=lambda node:node.cumulative)
-                                   Queue=deque(s)
-               else:
-                   no_target = True
-                   break
- 
-           z=z+len(Queue)
-           if(no_target == True):
-               print ("Nodes Expanded: "+str(nodes_generated))
-               print("Nodes Generated:" +str(z-1))
-               print("Max Nodes In Memory:" +str(w))
-               print ("Distance: Infinity")
-               print ("Route: None")
-           else:
-               print ("Nodes Expanded: "+str(nodes_generated))
-               print("Nodes Generated:" +str(z-1))
-               print("Max Nodes in Memory:" +str(w))
-               print ("Distance: " + str(nExpected.cumulative))
-               print ("Route: ")
-               path = []
-               path.append(target)
-               while nExpected.parents != None:
-                   path.append(nExpected.parents.name)
-                   nExpected = nExpected.parents
-               path.reverse()
-               i=0
-               while i < len(path)-1:
-                   for v in PATH:
-                       if path[i]==v[0] and path[i+1] == v[1]:
-                           print (v[0] +" to "+ v[1]+ " - "+v[2])
-                   i += 1
- 
-
+def main():
+	
+	dumpValues = True #set this to True to dump fringe and closed after every loop.
+	
+	#figure out if doing uninf or inf search
+	l = len(sys.argv)
+	if l == 4:
+		if dumpValues:
+			print 'Uninformed Search selected\n'
+		uSearch = True
+	elif l == 5:
+		if dumpValues:
+			print 'Informed Search selected\n'
+		uSearch = False
+	else:
+		print 'Incorrect number of arguments\n'
+		sys.exit()
+	
+	#read and store the input file as dictionary
+	map = readInput(sys.argv[1])
+	h = {}
+	if not uSearch:
+		#for inf search read and store heuristic as dictionary
+		h = readHeuristic(sys.argv[4])
+	
+	#initialize fringe with start node and closed as empty
+	fringe = []
+	if uSearch:
+		fringe.append(node(None, sys.argv[2], 0, 0, 0,uSearch))
+	else:
+		fringe.append(node(None, sys.argv[2], 0, 0, h[sys.argv[2]],uSearch))
+	closed = []
+	nProc = 0 #number of nodes Processed
+	sFringe = 1 #max size of fringe
+	nGen = 0 #number of nodes generated
+	if dumpValues:
+		print 'Nodes Processed: '+str(nProc)
+		dump(fringe,closed)
+	
+	#main loop of search
+	l = len(fringe)
+	while l > 0:
+		n = fringe.pop(0)
+		nProc = nProc + 1
+		if dumpValues:
+			print 'Processing Node: '+str(nProc)
+		#take node with cheapest g/f value
+		if n.state == sys.argv[3]: #check if goal state
+			if dumpValues:
+				print 'Goal Found: '+str(n)+'.\n\nOutput Generated:\n'
+			print 'nodes processed: '+str(nProc)
+			print 'nodes generated: '+str(nGen)
+			print 'max size of fringe: '+str(sFringe)
+			genRoute(n,map)
+			sys.exit()
+		else:
+			if n.state not in closed: #check if state already in closed
+				if dumpValues:
+					print 'Generating successors to '+n.state
+				closed.append(n.state)
+				succ = expand(n,map,h) #generate successors
+				for s in succ:
+					fringe.append(s) #add succesors to queue
+					nGen = nGen + 1
+				fringe.sort()#sort the queue
+			else:
+				if dumpValues:
+					print n.state + ' is alread in closed. No successors'
+		l = len(fringe)
+		if l > sFringe:
+			sFringe = l
+		if dumpValues:
+			print 'Nodes generated so far: '+str(nGen)
+			print 'Current Fringe size: '+str(l)
+			dump(fringe,closed)
+	#following code only runs if Fringe is empty i.e. no path possible.
+	if dumpValues:
+		print 'Fringe Empty. Goal Not Found. Generating Output\n'
+	print 'nodes processed: '+str(nProc)
+	print 'nodes generated: '+str(nGen)
+	print 'max size of fringe: '+str(sFringe)
+	print '\ndistance: infinity\nroute:\nnone'
 
 if __name__ == "__main__":
-            w=0
-            source = sys.argv[2]
-            destination = sys.argv[3]
-            
-            if len(sys.argv) >4:
-                u=informed_search()
-            else:
-                u=uninformed_search()
-            u.find_route(source, destination)
- 
+	main()
+	
